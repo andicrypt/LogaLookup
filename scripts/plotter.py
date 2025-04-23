c571c23858e93b55008d97942910e91df76c9276
andicrypt <keepgoing0901@gmail.com>
2025-04-24 01:28:23 +0700

    draw bench result

--- scripts/plotter.py
+++ scripts/plotter.py
@@ -1,14 +1,16 @@
 from matplotlib import pyplot as plt
 from matplotlib import font_manager as fm, rc
+import numpy as np
 
 font_legend = fm.FontProperties(family = 'Times New Roman')
 tnr = 'Times New Roman'
 font = {'family': tnr}
 
+rc('text', usetex=True)
 rc('font', **font)
 
 del fm.weight_dict['roman']
-fm._rebuild()
+# fm._rebuild()
 
 def get_Time (file):
     lines = [list(map(int, line.split())) for line in file.split("\n")[:-1]]
@@ -24,28 +26,31 @@ def plot_comparison(num_threads, gate = "vanilla"):
     loga_nv, loga_time = get_Time (loga)
     plk_nv, plk_time = get_Time (plk)
     plt.clf()
-    plt.title(f"SNARK + lookup proof generation - {num_threads} thread(s)", **font, fontweight='bold')
-    plt.ylabel("Time (microseconds)", **font)
-    plt.xlabel("Number of variables", **font)
+    plt.title(f"HyperPlonk with Lookup subroutine proof generation - {num_threads} thread(s)", **font, fontweight='bold')
+    plt.ylabel("Time (ns)", **font)
+    plt.xlabel(r"$\mu$", **font)
     plt.xticks(fontname=tnr)
     plt.yticks(fontname=tnr)
+    plt.grid(True)
     plt.plot(loga_nv, loga_time, label="Logalookup")
     plt.plot(plk_nv, plk_time, label="Plookup")
     plt.legend(prop=font_legend)
     
     plt.savefig(f"plot-compare-{num_threads}-threads.png")
 
-def plot_multithread(threads, lk = "logalk", gate = "vanilla"):
+def plot_multithread(threads, lk = "logalk", gate = "jellyfish"):
     plt.clf()
-    lk_str = "Logalookup" if lk == "logalk" else "Plookup"
-    plt.title(f"SNARK+{lk_str} proof generation - Multithread", **font, fontweight='bold')
-    plt.ylabel("Time (microseconds)", **font)
-    plt.xlabel("Number of variables", **font)
+    # lk_str = "LogaLookup" if lk == "logalk" else "Plookup"
+    # plt.title(f"HyperPlonk with {lk_str} proof generation - Multithread", **font, fontweight='bold')
+    plt.ylabel("Time (ns)", **font)
+    plt.xlabel(r"$\mu$", **font)
     plt.xticks(fontname=tnr)
     plt.yticks(fontname=tnr)
+    plt.grid(True)
     for nt in threads:
         file = open(f"hyperplonk/{lk} {gate} threads {nt}.txt").read()
         nv, time = get_Time (file)
+        time = [t * 1000 for t in time]
         plt.plot(nv, time, label=f"{nt} threads")
     plt.legend(prop=font_legend)
     plt.savefig(f"plot-multithread-{lk}.png")
@@ -57,11 +62,12 @@ def plot_comparison_lookup_subroutines(num_threads):
     loga_nv, loga_time = get_Time (loga)
     plk_nv, plk_time = get_Time (plk)
     plt.clf()
-    plt.title(f"Lookup proof generation - {num_threads} thread(s)", **font, fontweight='bold')
-    plt.ylabel("Time (nanoseconds)", **font)
-    plt.xlabel("Number of variables", **font)
+    # plt.title(f"{num_threads} thread(s)", **font, fontweight='bold')
+    plt.ylabel("Time (ns)", **font)
+    plt.xlabel(r"$\mu$", **font)
     plt.xticks(fontname=tnr)
     plt.yticks(fontname=tnr)
+    plt.grid(True)
     plt.plot(loga_nv, loga_time, label="Logalookup")
     plt.plot(plk_nv, plk_time, label="Plookup")
     plt.legend(prop=font_legend)
@@ -70,12 +76,13 @@ def plot_comparison_lookup_subroutines(num_threads):
 
 def plot_multithread_lookup_subroutine(threads, lk = "logalk"):
     plt.clf()
-    lk_str = "Logalookup" if lk == "logalk" else "Plookup"
-    plt.title(f"{lk_str} subroutine proof generation - Multithread", **font, fontweight='bold')
-    plt.ylabel("Time (nanoseconds)", **font)
-    plt.xlabel("Number of variables", **font)
+    # lk_str = "LogaLookup" if lk == "logalk" else "Plookup"
+    # plt.title(f"{lk_str} subroutine proof generation - Multithread", **font, fontweight='bold')
+    plt.ylabel("Time (ns)", **font)
+    plt.xlabel(r"$\mu$", **font)
     plt.xticks(fontname=tnr)
     plt.yticks(fontname=tnr)
+    plt.grid(True)
     for nt in threads:
         file = open(f"subroutines/{lk} {nt} threads.txt").read()
         nv, time = get_Time (file)
@@ -83,9 +90,9 @@ def plot_multithread_lookup_subroutine(threads, lk = "logalk"):
     plt.legend(prop=font_legend)
     plt.savefig(f"plot-multithread-{lk}-subroutine.png")
 
-for nt in [1,2,4,8]:
+for nt in [8,16,24,32]:
     plot_comparison(nt)
-plot_multithread([1,2,4,8])
-for nt in [1,2,4,8]:
+plot_multithread([8,16,24,32])
+for nt in [8,16,24,32]:
    plot_comparison_lookup_subroutines(nt)
-plot_multithread_lookup_subroutine([1,2,4,8])
+plot_multithread_lookup_subroutine([8,16,24,32])
